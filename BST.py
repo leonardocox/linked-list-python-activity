94755741304365aed2af24d48ba193f84c664c5a
leonardocox <leonardocx0@gmail.com>
2022-11-08 22:08:24 -0500

    Added making a BST

---
+++ BST.py
@@ -0,0 +1,96 @@
+class BSTNode:
+    def __init__(self, data=None, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return str(self.data)
+
+    def __repr__(self) -> str:
+        return str(self.data)
+
+
+class BST:
+    def __init__(self, root=None):
+        self.root = root
+        self.contents = []
+
+    def __str__(self) -> str:
+        if self.root == None:
+            return "The tree is empty"
+        else:
+            self.output = ""
+            self.print_tree(node=self.root)
+            return self.output
+
+    def __repr__(self) -> str:
+        return BST.__str__(self)
+
+    def print_tree(self, node, level=0):
+        if node != None:
+            self.print_tree(node.right, level + 1)
+            self.output += '' * 4 * level + '->' + str(node.data) + ''
+            self.print_tree(node.left, level + 1)
+
+    def add(self, node):
+        if type(node) != int and type(node) != BSTNode:
+            raise ValueError("You must only use ints or BSTNodes here")
+        if type(node) == int:
+            node = BSTNode(node)
+
+        if node.data in self.contents:
+            return
+
+        if self.root == None:
+            self.root = node
+            self.contents.append(node.data)
+            return
+
+        self.add_node(self.root, node)
+
+    def add_node(self, cur_node, new_node):
+        if new_node.data > cur_node.data:
+            if cur_node.right == None:
+                cur_node.right = new_node
+                self.contents.append(new_node.data)
+                return
+            else:
+                self.add_node(cur_node.right, new_node.data)
+        else:
+            if cur_node.left == None:
+                cur_node.left = new_node
+                self.contents.append(new_node.data)
+                return
+            else:
+                self.add_node(cur_node.left, new_node.data)
+
+
+def main():
+
+    node8 = BSTNode(8)
+    node3 = BSTNode(3)
+    node10 = BSTNode(10)
+    node1 = BSTNode(1)
+    node6 = BSTNode(6)
+    node14 = BSTNode(14)
+    node4 = BSTNode(4)
+    node7 = BSTNode(7)
+    node13 = BSTNode(13)
+
+    bst = BST()
+    bst.add(node8)
+    bst.add(node3)
+    bst.add(node10)
+    bst.add(node1)
+    bst.add(node6)
+    bst.add(node14)
+    bst.add(node4)
+    bst.add(node7)
+    bst.add(node13)
+
+    print(bst)
+
+
+if __name__ == "__main__":
+    main()
